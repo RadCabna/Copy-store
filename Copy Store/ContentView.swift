4b4118772a383f4db41e52013e60ca1440c64d03
Radcabna <radcabna@gmail.com>
2025-12-23 06:53:58 +0300

    Initial Commit

---
+++ Copy Store/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Copy Store
+//
+//  Created by Алкександр Степанов on 23.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Copy Store/Copy_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Copy_StoreApp.swift
+//  Copy Store
+//
+//  Created by Алкександр Степанов on 23.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Copy_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
